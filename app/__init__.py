65497601fe54c74180cb69e9b6f7b0028d43d544
zhangteng <fuyunbiyi@gmail.com>
2016-08-04 19:50:40 +0800

    register extensions

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,20 @@
 from flask import Flask
-from .utils import RedisSessionInterface
+from app.utils import RedisSessionInterface
+from app.extensions import (
+    db,
+    cache,
+    celery,
+)
 
 
 def create_app():
     app = Flask(__name__)
     app.session_interface = RedisSessionInterface()
+    register_extensions(app)
     return app
+
+
+def register_extensions(app):
+    db.init_app(app)
+    cache.init_app(app)
+    celery.config_from_object(app.config)
