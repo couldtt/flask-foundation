6dadef9f70fd453f3c9014a2dc77d2af81257c7a
zhangteng <fuyunbiyi@gmail.com>
2016-08-05 15:40:45 +0800

    init middleware

---
+++ app/middlewares/frequency.py
@@ -0,0 +1,8 @@
+
+class RequestFrequencyMiddleware(object):
+    def __init__(self, app):
+        self.app = app
+
+    def __call__(self, environ, start_response):
+        response = self.app(environ, start_response)
+        return response
---
+++ app/middlewares/timing.py
@@ -0,0 +1,18 @@
+import time
+
+
+class ResponseTimingMiddleware(object):
+    def __init__(self, app):
+        self.app = app
+
+    def __call__(self, environ, start_response):
+        start_time = time.time()
+        response = self.app(environ, start_response)
+        response_time = (time.time() - start_time) * 1000
+        data = {
+            'path': environ.get('PATH_INFO'),
+            'method': environ.get('REQUEST_METHOD'),
+            'time': int(response_time)
+        }
+
+        return response
