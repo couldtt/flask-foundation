63f5dbceb89a1c2963b6b3d8f24d02196ae07e8a
zhangteng <fuyunbiyi@gmail.com>
2016-08-08 13:51:13 +0800

    import redis

--- app/extensions.py
+++ app/extensions.py
@@ -2,9 +2,11 @@ from flask_cache import Cache
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from celery import Celery
+from redis import StrictRedis
 
 
 cache = Cache()
 db = SQLAlchemy()
 bcrypt = Bcrypt()
 celery = Celery()
+redis = StrictRedis()
